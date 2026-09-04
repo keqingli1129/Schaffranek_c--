a96a95791e6d031148757a4796a2fba9354a1aba
keqing Li <keqingli1129@gmail.com>
2026-09-04 16:28:44 -0500

    first commit

---
+++ build/CMakeFiles/Schaffranek_c++.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Schaffranek_c++.dir/main.cpp.o"
+  "CMakeFiles/Schaffranek_c++.dir/main.cpp.o.d"
+  "Schaffranek_c++"
+  "Schaffranek_c++.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Schaffranek_c++.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/keqingli1129/Schaffranek_c++
+# Build directory: /home/keqingli1129/Schaffranek_c++/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
